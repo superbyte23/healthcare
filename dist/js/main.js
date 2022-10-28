84acec0263c7f4d8f06c753d4b50539c1a426835
John Cañete <50263189+superbyte23@users.noreply.github.com>
2022-10-28 11:14:45 +0800

    Updated index.html. Resized image

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,7 @@
 // navbar change backgorund when scroll
 window.onscroll = function() {scrollFunction() }; 
 function scrollFunction() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) { 
+	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) { 
 		document.getElementById("navbar").classList.add("navbar-dark", "bg-dark");
 		document.getElementById("offcanvasNavbar").classList.add("bg-dark", "text-light");
 	}else{
